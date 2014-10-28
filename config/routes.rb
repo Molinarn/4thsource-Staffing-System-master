94c1a4655692651749b7df4ec2a3f96da0ebd9ce
rmolina <ricardo.molina@4thsource.com>
2014-10-28 11:08:26 -0600

    Resume Index View CheckPoint

--- config/routes.rb
+++ config/routes.rb
@@ -300,7 +300,7 @@ StaffingApp::Application.routes.draw do
   ## Candidates Profiles
 
   ##Added
-  #match "/candidates/:id/candidate_profiles" => 'candidate_profiles#index'
+  match "/candidates/:id/candidate_profiles" => 'candidate_profiles#index'
   match "/candidates/:id/candidates_profiles" => 'candidates_profiles#index'
   match "/candidates/:id/candidates_profiles/save" => 'candidates_profiles#save'
   match "/candidates_profiles/:candidates_profile_id/delete" => 'candidates_profiles#delete'
