2cbf321bbe50f015e75c66475f656bd2002ae076
rmolina <ricardo.molina@4thsource.com>
2014-10-29 17:19:06 -0600

    Languages comboBox enabled

--- app/controllers/candidate_languages_controller.rb
+++ app/controllers/candidate_languages_controller.rb
@@ -2,7 +2,9 @@ class CandidateLanguagesController < ApplicationController
   before_filter :authenticate
   
    def index
-    
+
+     puts "\ncandidate_languages#index".green
+
     id = params[:id] unless params.blank?
     if !current_candidate.admin_flag.nil?
       @candidate = Candidate.find(id)
@@ -10,15 +12,22 @@ class CandidateLanguagesController < ApplicationController
     else
       @candidate = current_candidate
       @error  = current_candidate.errors
-    end  
-    
+    end
+
     @total_languages = @candidate.candidate_languages
     @language = CandidateLanguage.new
    end
+
   def new
-    
-    id = params[:candidate_language]['idurl'] 
-    unless params.blank?
+
+    params.each do |p|
+      puts "#{p}".cyan
+    end
+
+    puts "\ncandidate_languages#new".green
+
+    id = params[:candidate_language][:idurl]
+    #unless params.blank?
     #id=params[:candidate_id] unless params.blank?
     if !current_candidate.admin_flag.nil?
       @candidate = Candidate.find(id)
@@ -26,47 +35,56 @@ class CandidateLanguagesController < ApplicationController
     else
       @candidate = current_candidate
       @error  = current_candidate.errors
-    end  
-    @candidate_language = @candidate.candidate_languages.build(params[:candidate_language])
-    language = Language.new 
+    end
+    #@candidate_language = @candidate.candidate_languages.build(params[:candidate_language])
 
-     if (params[:language_notinlist])
+    language = Language.new
+    #@candidate_language = @candidate.candidate_languages.new
 
-          if(!params[:lang_name].empty?)
-            language.name = params[:lang_name]
-            if (language.name.size > 50)
-              flash[:notice] = "Language name could not be more than 50 characters"
-              language = nil
-            else
-              language.save
-            end
-          else
-            flash[:notice] = "Please provide a valid language name"
-            language = nil
-          end
+    #If add manually is selected and the language is not in the list
+    if params[:language_notinlist] && Language.where("name = ?",params[:lang_name]).count == 0
 
-     elsif params[:language_id].nil?
-         flash[:notice] = "If language is not in the list, add it manually."
-         @language = CandidateLanguage.new
-         language = nil
-       else
-         language = Language.find(params[:language_id])
-       end
-       
-     
-     end
-     @candidate_language.language = language
-    if (language != nil )
+      puts "\nparams[:language_notinlist]: #{params[:language_notinlist]}".magenta
+      puts "\nname = ?,params[:lang_name]: #{Language.where("name = ?",params[:lang_name]).count}".magenta
+
+      #if !params[:lang_name].empty?
+        language.name = params[:lang_name]
+        if language.name.size > 50
+          flash[:notice] = "Language name could not be more than 50 characters"
+          language = nil
+        else
+          language.save
+        end
+      #else
+       # flash[:notice] = "Please provide a valid language name"
+       # language = nil
+      #end
+
+    #elsif params[:id].nil?
+      #flash[:notice] = "If language is not in the list, add it manually."
+      #@language = CandidateLanguage.new
+      #language = nil
+    else
+      language = Language.find(params[:candidate_language][:id])
+    end
+
+    if language != nil
+
+      @candidate_language = @candidate.candidate_languages.build(params[:candidate_language])
+      @candidate_language.language = language
+
       if @candidate_language.save
         flash[:success] = "languages was saved successfully."
       else
         language.destroy
         flash[:notice] = "An error occurred while the system save the languages#{@candidate_language.errors.as_json}"
       end
-    end 
-    redirect_to request.referer 
+
+      redirect_to request.referer
+
+    end
   end
-  
+
   def destroy
     CandidateLanguage.find(params[:candidate_language]).destroy
     redirect_to request.referer
@@ -91,7 +109,7 @@ class CandidateLanguagesController < ApplicationController
       if @language.save
         flash[:success] = "Language was saved successfully."
         @projects_items = @candidate.projects
-        redirect_to File.join('/candidates/', @candidate.id.to_s(), '/resume/languages')
+        redirect_to File.join('/candidates/', @candidate.id.to_s, '/resume/languages')
       else
         flash[:notice] = "An error occurred while the system save the language."
       end
